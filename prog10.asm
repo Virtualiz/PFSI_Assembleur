71a3eb8522fe14c18d5d9649e4a441588340e03c
Virtualiz <sinteff.bruno@gmail.com>
2016-12-12 09:34:47 +0100

    test with seuil in parameter

--- prog10.asm
+++ prog10.asm
@@ -12,14 +12,16 @@ DEBUT LDW SP,#STACKA
     LDW R2,@INA //lire val dans parametre (R2)
     STW R2, -(SP) //store R2
     STW SP,BP // BP <= SP (parameter address)
+    LDW R2,#SEUIL
+    STW R2, -(SP) // store seuil in stack
     JSR @DISTA
-    ADQ 2,SP // downgrade stack pointer
+    ADQ 4,SP // downgrade stack pointer
     STW R0,@OUTA //ecrire la sortie dans outa
     JMP #DEBUT-$-2
 
 DISTA STW R1,-(SP) //PUSH R1
-    LDW R2,(BP) //Load R2 via BP
-    LDW R1,#SEUIL //charge le seuil dans R1
+    LDW R2,(BP)+ //Load R2 via BP
+    LDW R1,(BP) //charge le seuil dans R1 via BP
     SUB R2,R1,R0 // soustrait par le seuil
     JSR @ABSA
     SRA R0,R0 // div par 2
