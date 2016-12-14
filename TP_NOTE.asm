ff0aea8101ba12aac5c7df71d10355a071afe640
Virtualiz <sinteff.bruno@gmail.com>
2016-12-14 08:56:59 +0100

    changement avec lecture CNTMA a chaque tour de boucle

--- TP_NOTE.asm
+++ TP_NOTE.asm
@@ -13,9 +13,11 @@ DEBUT LDW SP,#STACKA // initialisation du stackpointer
     LDW R0,#INIT // on charge la valeur d'init adns R0
     STW R0,@CNTMA // initialisation de CNTMA
     LDW R1,#MAX // on charge la valeur max dans R1
-LOOP ADQ INC, R0 // incrementation de R0
+LOOP LDW R0, @CNTMA // on charge CNTMA dans R0
+    ADQ INC, R0 // incrementation de R0
     CMP R0, R1 // on compare R0 et R1
     BLW STORE-$-2 // si R0 <= MAX, on va a STORE
     LDW R0, #INIT // sinon, on reinitialise R0 a 0
+    STW R0, @CNTMA // on charge R0 a l'adresse CNTMA
 STORE STW R0, @CNTMA // on charge R0 a l'adresse CNTMA
     JMP #LOOP-$-2 // jump au debut de loop
